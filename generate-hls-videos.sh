9894449b4fa42d427a71c6cdd547cfd404e20d60
æLtorio <aeltorio@universidadatlantico.org>
2024-07-01 14:23:41 +0200

    chore: Add helper functions for improved code organization

--- generate-hls-videos.sh
+++ generate-hls-videos.sh
@@ -58,6 +58,18 @@
 # 	videoBucket: string;
 # };
 
+# Function to check if required binaries are installed
+# This function checks if the following binaries are installed:
+# - ffmpeg: Required for video conversion and manipulation
+# - ffprobe: Required for video analysis and metadata extraction
+# - bc: Required for performing mathematical calculations
+# - uuidgen: Required for generating unique identifiers
+# - jq: Required for JSON parsing and manipulation
+# - mc: Required for interacting with MinIO object storage
+# If any of the binaries are not found, an error message is displayed.
+# Returns:
+# - 0 if all required binaries are found
+# - 1 if any of the required binaries are not found
 test_required_binaries() {
     if ! command -v ffmpeg &>/dev/null; then
         echo "ffmpeg could not be found see https://ffmpeg.org/download.html"
@@ -87,6 +99,21 @@ test_required_binaries() {
 }
 
 # This function is used to set or remove mc aliases based on the s3-config.json file used by hls-video-s3-proxy on cloudflare
+#
+#    Defines MinIO client aliases based on the provided action and s3-config.json file.
+#
+#    Args:
+#        action (str): The action to perform. Must be one of "set", "rm", or "remove".
+#        json_file (str): The path to the s3-config.json file used by hls-video-s3-proxy on Cloudflare.
+#
+#    Returns:
+#        None
+#
+#    Raises:
+#        None
+#
+#    Example:
+#        define_mc_aliases "set" "s3-config.json"
 define_mc_aliases() {
     test_required_binaries
     action=$1
@@ -121,6 +148,15 @@ define_mc_aliases() {
 }
 
 # This function is used to publish the HLS videos to the remote s3 bucket
+# Publishes HLS videos to a remote S3 bucket.
+#
+# Args:
+#   json_file (str): The path to the s3-config.json file used by hls-video-s3-proxy on cloudflare.
+#   bucket_name (str): The name of the S3 bucket.
+#   local_folder (str): The local folder containing the HLS videos.
+#
+# Returns:
+#   None
 publish_hls_videos(){
     test_required_binaries
     usage="usage: publish_hls_videos s3-config.json bucket_name local_folder"
@@ -166,16 +202,29 @@ get_mc_aliases(){
     done
 }
 
+# Function to get the name of the current shell
+# Returns the name of the shell
 get_shell_name() {
     shell_name=$(basename "$SHELL")
     echo "$shell_name"
 }
 
+
+# Function to get the width and height ratio of a video file.
+# Parameters:
+#   $1: The path to the video file.
+# Returns:
+#   The width and height ratio of the video file.
 get_video_ratio() {
     video_file=$1
     ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$video_file"
 }
 
+# Function to get the frame rate of a video file
+# Arguments:
+#   $1: video_file - the path to the video file
+# Returns:
+#   The frame rate of the video file
 get_video_frame_rate() {
     video_file=$1
     rate_base=$(ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of csv=s=x:p=0 "$video_file" 2>/dev/null)
@@ -183,6 +232,11 @@ get_video_frame_rate() {
     echo $rate | tail -n 1
 }
 
+# Function to get the numeric ratio of a video file
+# Arguments:
+#   $1: video_file - The path to the video file
+# Returns:
+#   The numeric ratio of the video file
 get_video_ratio_numeric() {
     video_file=$1
     ratio_text=$(get_video_ratio "$video_file")
@@ -192,11 +246,21 @@ get_video_ratio_numeric() {
     echo $ratio
 }
 
+# Function to get the number of lines in a video file
+# Parameters:
+#   $1: video_file - the path to the video file
+# Returns:
+#   The number of lines in the video file
 get_video_nb_lines() {
     video_file=$1
     ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "$video_file"
 }
 
+# Function to generate multi-resolution HLS videos for files matching a given pattern.
+# Parameters:
+#   - pattern: The pattern to match the files. Example: /path/to/videos/*.mp4
+# Usage: generate_multi_resolution_hls_for_pattern_files pattern
+# Example: generate_multi_resolution_hls_for_pattern_files /path/to/videos/*.mp4
 generate_multi_resolution_hls_for_pattern_files(){
     pattern=$1
     if [[ -z $pattern ]]; then
@@ -223,31 +287,57 @@ generate_multi_resolution_hls_for_pattern_files(){
     unset pattern _filename _dirname
 }
 
+# Function to generate multi-resolution HLS videos
+# This function takes a video file as input and encodes it into multiple resolutions using FFmpeg.
+# It generates HLS playlists and segments for each resolution, as well as a master playlist file.
+# The function also extracts a frame from the video and creates a JSON file with the filenames of the generated videos.
+# Parameters:
+#   - $1: Path to the input video file
+# Usage: generate_multi_resolution_hls /path/to/video.mp4
 generate_multi_resolution_hls() {
+    # Check if required binaries are available
     test_required_binaries
-    # nombre de secondes par segment
+
+    # Set the segment length for HLS playlists
     HLS_SEGMENT_LENGTH=6
+
+    # Get the shell type (zsh or bash)
     SHELLTYPE=$(get_shell_name)
+
+    # Get the input video file path
     input=$1
+
+    # Check if input video file is provided
     if [[ -z $input ]]; then
         echo "Please provide a video file"
         echo "Usage: generate_multi_resolution_hls /path/to/video.mp4"
         echo "Example: generate_multi_resolution_hls /path/to/video.mp4"
         return
     fi
+
+    # Get the number of lines in the source video
     NBLINES=$(get_video_nb_lines "$input")
+
+    # Get the video ratio (numeric value)
     RATIO=$(get_video_ratio_numeric "$input")
+
+    # Get the frame rate of the video
     FRAME_RATE=$(get_video_frame_rate "$input")
+
+    # Get the base name of the input video file
     base_name="$(basename "$input" .mp4)"
+
+    # Print shell type, number of lines, and video ratio
     echo "Shell type: $SHELLTYPE"
     echo "Number of lines in source video: $NBLINES"
     echo "Video ratio: $RATIO"
 
-    # UUID="8DCDE049-2F20-4EEA-BE4C-CE6504BC2BAA"
-    # Select the closest resolution based on the given ratio
+    # Declare an associative array to store resolutions based on the video ratio
     declare -A resolutions
+
+    # Select resolutions based on the video ratio
     if (( $(echo "$RATIO < 1" | bc -l) )); then
-        # résolutions 9:16
+        # Resolutions for 9:16 ratio
         echo "Using 9:16 resolutions"
         resolutions[240]="136x240"
         resolutions[640]="360x640"
@@ -257,7 +347,7 @@ generate_multi_resolution_hls() {
         resolutions[2560]="1440x2560"
         resolutions[3840]="2160x3840"
     elif (( $(echo "$RATIO > 1.5 " | bc -l) )); then
-        # résolutions 16:9
+        # Resolutions for 16:9 ratio
         echo "Using 16:9 resolutions"
         resolutions[136]="240x136"
         resolutions[240]="426x240"
@@ -267,7 +357,7 @@ generate_multi_resolution_hls() {
         resolutions[1440]="2560x1440"
         resolutions[2160]="3840x2160"
     elif (( $(echo "1.1 < $RATIO <= 1.5 " | bc -l) )); then
-        # résolutions 4:3
+        # Resolutions for 4:3 ratio
         echo "Using 4:3 resolutions"
         resolutions[136]="204x136"
         resolutions[240]="320x240"
@@ -277,7 +367,7 @@ generate_multi_resolution_hls() {
         resolutions[1440]="1920x1440"
         resolutions[2880]="3840x2880"
     else
-        # résolutions 1:1
+        # Resolutions for 1:1 ratio
         echo "Using square resolutions"
         resolutions[136]="136x136"
         resolutions[240]="240x240"
@@ -289,50 +379,58 @@ generate_multi_resolution_hls() {
         resolutions[3840]="3840x3840"
     fi
 
-    # Extraire les clés
+    # Extract the keys from the resolutions associative array
     if [[ $SHELLTYPE == "zsh" ]]; then
-        # Pour Zsh
+        # For Zsh
         keys=("${(@k)resolutions}")
     else
-        # Pour Bash
+        # For Bash
         keys=("${!resolutions[@]}")
     fi
 
-    # Trier les clés
+    # Sort the keys in ascending order
     unset sorted_keys
     IFS=$'\n' sorted_keys=($(printf "%s\n" "${keys[@]}" | sort -n))
     unset IFS
 
-    # Créer un tableau associatif pour les résolutions inférieures ou égales à la résolution de la vidéo
+    # Create a new associative array for resolutions that are less than or equal to the video resolution
     declare -A sub_resolutions
     for key in "${sorted_keys[@]}"; do
         if [[ $key -le $NBLINES ]]; then
             sub_resolutions[$key]="${resolutions[$key]}"
         fi
     done
-        # Extraire les clés
+
+    # Extract the keys from the sub_resolutions associative array
     if [[ $SHELLTYPE == "zsh" ]]; then
-        # Pour Zsh
+        # For Zsh
         keys=("${(@k)sub_resolutions}")
     else
-        # Pour Bash
+        # For Bash
         keys=("${!sub_resolutions[@]}")
     fi
-    # Trier les clés du nouveau tableau associatif
+
+    # Sort the keys of the new associative array
     IFS=$'\n' sorted_keys=($(printf "%s\n" "${keys[@]}" | sort -n))
     unset IFS
+
+    # Print the resolutions and their corresponding keys
     for key in "${sorted_keys[@]}"; do
         echo "Key: $key Value: ${resolutions[$key]}"
     done
 
+    # Check if UUID is empty
     if [[ -z $UUID ]]; then
-
+        # Generate UUID if the base directory already exists, otherwise use the base name
         if [[ -d "${base_name}" ]]; then
             UUID=$(uuidgen)
         else
             UUID=${base_name}
         fi
+
+        # Create a directory with the UUID
         mkdir -p ./$UUID
+
         # Encode video in multiple resolutions
         for key in "${sorted_keys[@]}"; do
             resolution_hxv=${resolutions[$key]}
@@ -342,18 +440,23 @@ generate_multi_resolution_hls() {
             ratio=$(echo "scale=3; $resolutionv / $resolutionh" | bc)
             echo "Ratio: $ratio"
             echo "Encoding video in $resolution_hxv resolution"
-            # encode video according to the rfc6381 CODECS="mp4a.40.2,avc1.100.40"
+
+            # Encode video using FFmpeg
             ffmpeg -i "$input" \
                 -c:v libx264 -profile:v high -level 4.0 -s $resolution_hxv \
                 -c:a aac -b:a 128k \
                 -start_number 0 -hls_time $HLS_SEGMENT_LENGTH -hls_list_size 0 -hls_segment_type fmp4 -f hls \
                 "${UUID}/${base_name}_${resolutionh}p.m3u8"
+
+            # Rename the init.mp4 file to match the resolution
             mv "${UUID}/init.mp4" "${UUID}/${base_name}_${resolutionh}p.mp4"
+
+            # Update the m3u8 file to reference the renamed video file
             sed -i '' -e 's/init.mp4/'"${base_name}_${resolutionh}p.mp4"'/g' "${UUID}/${base_name}_${resolutionh}p.m3u8"
+
             echo "************"
-            # echo "please hit enter to continue"
-            # read
         done
+
         # Extract a frame from the video
         fmpeg -i "$input" -ss 00:00:01 -vframes 1 "${UUID}/${base_name}_poster.jpg"
 
@@ -362,6 +465,7 @@ generate_multi_resolution_hls() {
         echo "#EXT-X-VERSION:7" >>"${UUID}/${base_name}_master.m3u8"
         echo "## Created by Ronan Le Meillat" >>"${UUID}/${base_name}_master.m3u8"
 
+        # Add stream information to the master playlist file
         for key in "${sorted_keys[@]}"; do
             resolution_hxv=${resolutions[$key]}
             resolutionh=$(echo $resolution_hxv | sed 's/x.*//')
@@ -370,7 +474,9 @@ generate_multi_resolution_hls() {
             echo "${base_name}_${resolutionh}p.m3u8" >>"${UUID}/${base_name}_master.m3u8"
         done
     fi
+
     wait
+
     # Generate JSON file with filenames
     multiresolution_json=()
     generated_resolutions=()
@@ -389,6 +495,8 @@ generate_multi_resolution_hls() {
         echo "Current resolution: $current_resolution"
         multiresolution_json+=$current_resolution
     done
+
+    # Create the JSON file with the master playlist, resolutions, and filenames
     echo "{\"master\":\"${base_name}_master.m3u8\",\"resolutions\":$(
         IFS=,
         echo "[${generated_resolutions[*]}]"
@@ -396,5 +504,7 @@ generate_multi_resolution_hls() {
         IFS=,
         echo "${multiresolution_json[*]}"
     )"}" >"${UUID}/keys.json"
+
+    # Clean up variables
     unset HLS_SEGMENT_LENGTH SHELLTYPE input NBLINES RATIO FRAME_RATE base_name UUID resolutions keys sorted_keys sub_resolutions generated_resolutions multiresolution_json resolution_hxv resolutionh resolutionv ratio
 }
